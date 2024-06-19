aacb6077dc117240dc2926cfde9a67443b4a01f6
cdjohnson97 <cdmilandou@gmail.com>
2024-06-19 16:14:45 +0200

    first initialisation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-5xl text-center'>it works !</p>
+      </div>
+    </>
+  )
+}
+
+export default App
